9a4fc6a963f881d861c6eab5791be21e4a96fab8
mohamedelserafy <elserafy02@gmail.com>
2021-03-02 11:34:09 +0200

    one last thing

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,11 +1,9 @@
 //plug in another middleware the will check if the user is logged in or not
 //all requests that are plugged in after this middleware will only be accessible if the user is logged in
-
+const ApiError = require('../errors/api-errors')
 function authenticate(req,res,next){
     if(!req.session ||!req.session.user){
-        const err = new Error('You shall not pass')
-        err.statusCode=401;
-        next(err)
+        next(ApiError.unauthorized('something went wrong'))
     }
     next();
 }
